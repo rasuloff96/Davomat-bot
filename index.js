ae4cbfefbd12deff7500a7095d8c0b5625d34d47
Asilbek <150068156+rasuloff96@users.noreply.github.com>
2024-02-13 11:17:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const TelegramBot = require('node-telegram-bot-api')
+const token = '6589441107:AAFSz0Pf9zMMaf6nej_5nkcdYZn2V-TB0zE'
+const { students } = require("./options")
+
+const bot = new TelegramBot(token, { polling: true })
+
+const password = "nazoratchi"
+
+
+bot.on("message", async (msg) => {
+    const text = msg.text
+    const chatId = msg.chat.id
+
+    if (text === "/start") {
+        return (
+            await bot.sendMessage(
+                chatId,
+                "Salom Davomat botga xush kelibsiz!!!"
+            ),
+            await bot.sendMessage(
+                chatId,
+                "Shaxsingizni anig'lash uchun parolni kiriting:"
+            )
+        )
+    }
+})
+
+bot.on("message", async (msg) => {
+    const text = msg.text
+    const chatId = msg.chat.id
+
+    if (text === password) {
+        return await bot.sendMessage(
+            chatId,
+            "Sinfingiz: 11-'L'",
+            students
+        )
+    }
+
+})
---
+++ options.js
@@ -0,0 +1,50 @@
+module.exports = {
+    students: {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: "Botrali Ergashev",
+                        callback_data: "Botirali Ergashev"
+                    },
+                    {
+                        text: "Samandar Eshonqulov",
+                        callback_data: "Samandar Eshonqulov"
+                    },
+                    {
+                        text: "Saidov Doston",
+                        callback_data: "Saidov Doston"
+                    },
+                ],
+                [
+                    {
+                        text: "Umarov Jamshid",
+                        callback_data: "Umarov Jamshid"
+                    },
+                    {
+                        text: "Garriev Potterbek",
+                        callback_data: "Garriev Potterbek"
+                    },
+                    {
+                        text: "Teshaev Bolta",
+                        callback_data: "Teshaev Bolta"
+                    }
+                ],
+                [
+                    {
+                        text: "Lochinov Burgut",
+                        callback_data: "Lochinov Burgut"
+                    },
+                    {
+                        text: "Kumushuv Oltinbek",
+                        callback_data: "Kumushuv Oltinbek"
+                    },
+                    {
+                        text: "Yolchiev Oltmishvoy",
+                        callback_data: "Yolchiev Oltmishvoy"
+                    }
+                ]
+            ]
+        }
+    }
+}
